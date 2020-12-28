2d883daaae1d0a47dc257cfcbe0f08243e16d37f
Betina <betina.andersson@minna.tech>
2020-12-28 20:57:34 +0100

    add design outline

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import {ChatBubble} from "./components/ChatBubble";
+import * as React from 'react';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 function App() {
+  const backgroundImage = 'https://raw.githubusercontent.com/DoodleScheduling/hiring-challenges/master/frontend-engineer/assets/Body%20BG.png';
+  const [value, setValue] = React.useState('');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={ {height: '100vh', marginTop: '-1rem', backgroundImage: `url(${backgroundImage})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', maxWidth: '640px'} }>
+      <div>
+        <div>
+          <ChatBubble />
+          <ChatBubble mainBubble />
+          <ChatBubble />
+        </div>
+        <div style={ {
+          backgroundColor: '#1F591A',
+          minHeight: '2rem',
+          position: 'fixed',
+          bottom: '0',
+          width: '100%',
+          padding: '0.5rem',
+          display: 'flex',
+          justifyContent: 'space-between'
+        } }>
+          <TextField
+            id="outlined-multiline-flexible"
+            multiline
+            rowsMax="4"
+            value={value}
+            onChange={handleChange}
+            variant="outlined"
+            placeholder="Message"
+            style={ {backgroundColor: '#FFFFFF', borderRadius: '4px'} }
+            fullWidth
+          />
+          <Button variant="contained" style={ {marginLeft: '1rem', marginRight: '1rem'} }>
+            Send
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
