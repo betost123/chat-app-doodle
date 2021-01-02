7cb45360a0af198c4aeb37f2f648c31afb3b2604
Betina <betina.andersson@minna.tech>
2021-01-02 18:58:20 +0100

    add date, and update messages when you sned a new message

--- src/App.js
+++ src/App.js
@@ -4,38 +4,67 @@ import * as React from 'react';
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import {getMessages, sendMessage} from "./utils";
+import List from "@material-ui/core/List";
 
 function App() {
   const backgroundImage = 'https://raw.githubusercontent.com/DoodleScheduling/hiring-challenges/master/frontend-engineer/assets/Body%20BG.png';
   const [value, setValue] = React.useState('');
-  const username = 'BetinaThroughApp';
+  const username = 'Me';
+  const bottomRef = React.createRef();
 
   const handleChange = (event) => {
     setValue(event.target.value);
+  };
+
+  const scrollToBottom = () => {
+    bottomRef.scrollIntoView({behavior: 'smooth'})
   };
 
   const [messages, setMessages] = React.useState([]);
 
   React.useEffect(() => {
     getMessages()
-        .then(setMessages)
+        .then((response) => {
+          setMessages(response);
+          scrollToBottom();
+
+        })
         .catch((error) => console.log(error));
   }, []);
 
   const onClickSend = () => {
     const newMessage = {message: value, author: username};
-    sendMessage(newMessage).catch((error) => console.log(error));
+    sendMessage(newMessage).then(() => {
+      getMessages()
+          .then((response) => {
+            setValue('');
+            setMessages(response);
+            scrollToBottom();
+
+          })
+          .catch((error) => console.log(error));
+    }).catch((error) => console.log(error));
+
   };
 
   return (
-    <div className="App" style={ {height: '100vh', marginTop: '-1rem', backgroundImage: `url(${backgroundImage})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', maxWidth: '640px'} }>
+    <div className="App" style={ {minHeight: '100vh', marginTop: '-1rem', backgroundImage: `url(${backgroundImage})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', maxWidth: '640px'} }>
       <div>
         <div>
-          {
-            messages.map((message, index) => (
-                <ChatBubble key={ index } author={ message.author } message={ message.message } mainBubble={ message.author === username } />
-            ))
-          }
+          <List style={{maxHeight: 'calc(100vh - 4rem)', overflow: 'auto'}}>
+            {
+              messages.map((message, index) => (
+                <ChatBubble
+                    key={ index }
+                    author={ message.author }
+                    message={ message.message }
+                    mainBubble={ message.author === username }
+                    timestamp = {message.timestamp}
+                />
+              ))
+            }
+            <div style={{ float:"left", clear: "both" }} ref={ bottomRef } />
+          </List>
         </div>
         <div style={ {
           backgroundColor: '#1F591A',
--- src/components/ChatBubble.js
+++ src/components/ChatBubble.js
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import Paper from "@material-ui/core/Paper";
 
-export const ChatBubble = ({mainBubble = false, author, message}) => {
+export const ChatBubble = ({mainBubble = false, author, message, timestamp}) => {
     const secondaryBubbleStyle = {backgroundColor: '#EEF3ED', maxWidth: '60%', margin: '1rem', padding: '1rem'};
     const mainBubbleStyle = {backgroundColor: '#E6F066', maxWidth: '60%', margin: '1rem', padding: '1rem', marginLeft: 'auto'};
+    const d = new Date(timestamp);
+    const date = d.toDateString() + ' ' + d.getHours() + ':' + d.getMinutes();
 
     return (
         <Paper elevation={ 4 } style={ mainBubble ? mainBubbleStyle : secondaryBubbleStyle }>
             <div style={ {color: '#8C928B', textAlign: 'left'} }>{ author }</div>
             <div style={ {textAlign: 'left'} }>{ message }</div>
-            <div style={ {color: '#8C928B', textAlign: 'left'} }>26 Dec 2020 12:39</div>
+            <div style={ {color: '#8C928B', textAlign: 'left'} }>{ date }</div>
         </Paper>
     )
 };
